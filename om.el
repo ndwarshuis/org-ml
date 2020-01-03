48662326df66aa68b144a7f38ea059475d0540ac
ndwarshuis <ndwar@yavin4.ch>
2020-01-03 15:32:17 -0500

    REF replace easter egg with legit commentary

--- om.el
+++ om.el
@@ -23,17 +23,44 @@
 
 ;;; Commentary:
 
-;; You are a...puppet
-;; You are a...puppet
-;; You are a...puppet
-;; You are a...puppet
+;; This is a functional API for org-mode primarily using the
+;; `org-element' library. `org-element.el' provides the means for
+;; converting an org buffer to a parse-tree data structure. This
+;; library contains functions to modify this parse-tree in a
+;; more-or-less 'purely' functional manner (with the exception of
+;; parsing from the buffer and writing back to the buffer). For the
+;; purpose of this package, the resulting parse tree is composed of
+;; 'nodes'
 
-;; I am a...puppet
-;; I am a...puppet
-;; I am a...puppet
-;; I am a...puppet
+;; This library exposes the following types of functions:
+;; - builder: build new nodes to be inserted into a parse tree
+;; - property functions: return either property values (get) or
+;;   nodes with modified properties (set and map)
+;; - children functions: return either children of nodes (get) or
+;;   return a node with modified children (set and map)
+;; - node predicates: return t if node meets a condition
+;; - pattern matching: return nodes based on a pattern that matches
+;;   the parse tree (and perform operations on those nodes depending
+;;   on the function)
+;; - parsers: parse a buffer (optionally at current point) and return
+;;   a parse tree
+;; - writers: insert/update the contents of a buffer given a parse
+;;   tree
 
-;; WE ARE ALL...PUPPETS
+;; This library has several unconventional uses of macros:
+;; - type checking: all public functions have a simple type-checking
+;;   mechanism
+;; - anaphoric forms: definition macros ending in a '*' will create
+;;   two definitions: one as defined in the source code and one as
+;;   an anaphoric macro (anaphoric forms are macro equivalents of
+;;   higher-order functions that take forms in the place of lambda
+;;   functions)
+;; - rest+keyval arguments: some functions require keyed arguments to
+;;   be used with the &rest parameter; this is not in `cl-macs' and
+;;   therefore requires a custom definition here
+
+;; For examples please see full documentation at:
+;; https://github.com/ndwarshuis/om.el
 
 ;;; Code:
 
