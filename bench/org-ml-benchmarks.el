724b160cecabec2e0447970ee91482a895226e5b
ndwarshuis <ndwar@yavin4.ch>
2020-12-01 21:58:17 -0500

    REF make native benchmarks more consistent

--- bench/org-ml-benchmarks.el
+++ bench/org-ml-benchmarks.el
@@ -21,19 +21,23 @@
 
 (require 'org-ml-bench-fw)
 
-(org-ml-defbench "read TODO" "\n* TODO headline" 10000
-  (while (outline-next-heading)
-    (org-get-todo-state))
+(org-ml-defbench "read TODO" "* TODO headline\n" 10000
+  (let ((next t))
+    (while next
+      (org-get-todo-state)
+      (setq next (outline-next-heading))))
   
   (->> (org-ml-get-headlines)
        (--map (org-ml-get-property :todo-keyword it))))
 
-(org-ml-defbench "TODO -> DONE" "\n* TODO headline" 1000
+(org-ml-defbench "TODO -> DONE" "* TODO headline\n" 1000
   (let ((org-log-done 'time)
         (org-todo-keywords '((sequence "TODO" "|" "DONE")))
-        (org-adapt-indentation nil))
-    (while (outline-next-heading)
-      (org-todo 'done)))
+        (org-adapt-indentation nil)
+        (next t))
+    (while next
+      (org-todo 'done)
+      (setq next (outline-next-heading))))
 
   (let ((planning (->> (org-ml-unixtime-to-time-long (float-time))
                        (org-ml-build-planning! :closed))))
@@ -42,19 +46,20 @@
            (org-ml-headline-set-planning planning)))))
 
 (org-ml-defbench "tag headline" "* headline\n" 1000
-  (progn
-    (org-set-tags '("A" "B" "C"))
-    (while (outline-next-heading)
-      (org-set-tags '("A" "B" "C"))))
+  (let ((next t))
+    (while next
+      (org-set-tags '("A" "B" "C"))
+      (setq next (outline-next-heading))))
 
   (org-ml-do-headlines*
     (org-ml-set-property :tags '("A" "B" "C") it)))
 
 (org-ml-defbench "schedule headline" "* headline\n" 1000
-  (let ((org-adapt-indentation nil))
-    (org-schedule nil "2000-01-01")
-    (while (outline-next-heading)
-      (org-schedule nil "2000-01-01")))
+  (let ((org-adapt-indentation nil)
+        (next t))
+    (while next
+      (org-schedule nil "2000-01-01")
+      (setq next (outline-next-heading))))
 
   (let ((pl (org-ml-build-planning! :scheduled '(2000 1 1))))
     (org-ml-do-headlines*
@@ -77,46 +82,49 @@
       (org-ml-map-property* :scheduled (org-ml-timestamp-shift 1 'day it) it) it)))
 
 (org-ml-defbench "set headline effort" "* headline\n" 1000
-  (let ((org-adapt-indentation nil))
-    (org-set-property "Effort" "0:05")
-    (while (outline-next-heading)
-      (org-set-property "Effort" "0:05")))
+  (let ((org-adapt-indentation nil)
+        (next t))
+    (while next
+      (org-set-property "Effort" "0:05")
+      (setq next (outline-next-heading))))
 
   (org-ml-do-headlines*
     (org-ml-headline-set-node-property "Effort" "0:05" it)))
 
 (org-ml-defbench "set checkboxes" "* headline [0/0]\n- [ ] one\n- [ ] two\n" 1000
-  (let ((org-adapt-indentation nil))
-    (org-toggle-checkbox)
-    (while (outline-next-heading)
-      (org-toggle-checkbox)))
+  (let ((org-adapt-indentation nil)
+        (next t))
+    (while next
+      (org-toggle-checkbox)
+      (setq next (outline-next-heading))))
 
   (org-ml-do-headlines*
     (->> (org-ml-match-map '(section plain-list item) #'org-ml-item-toggle-checkbox it)
          (org-ml-headline-update-item-statistics))))
 
 (org-ml-defbench "insert headline text" "* headline\n" 2500
-  (let ((org-adapt-indentation nil))
-    (save-excursion
-      (org-end-of-subtree)
-      (insert "\nsome text"))
-    (while (outline-next-heading)
+  (let ((org-adapt-indentation nil)
+        (next t))
+    (while next
       (save-excursion
         (org-end-of-subtree)
-        (insert "\nsome text"))))
+        (insert "\nsome text"))
+      (setq next (outline-next-heading))))
 
   (let ((para (org-ml-build-paragraph! "some text")))
     (org-ml-do-headlines*
       (org-ml-headline-set-section (list para) it))))
 
-(org-ml-defbench "set headline effort/TODO/scheduled" "\n* headline" 1000
+(org-ml-defbench "set headline effort/TODO/scheduled" "* headline\n" 1000
   (let ((org-log-done 'time)
         (org-todo-keywords '((sequence "TODO" "|" "DONE")))
-        (org-adapt-indentation nil))
-    (while (outline-next-heading)
+        (org-adapt-indentation nil)
+        (next t))
+    (while next
       (org-schedule nil "2000-01-01")
       (org-set-property "Effort" "0:05")
-      (org-todo 'todo)))
+      (org-todo 'todo)
+      (setq next (outline-next-heading))))
 
   (let ((pl (org-ml-build-planning! :scheduled '(2000 1 1))))
     (org-ml-do-headlines*
