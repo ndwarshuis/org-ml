d6e80d184c3b45f5655f450f22b948772f80bfea
ndwarshuis <ndwar@yavin4.ch>
2024-06-22 21:40:30 -0400

    ADD 29.3

---
+++ .emacs/29.3/straight/versions/default.el
@@ -0,0 +1,11 @@
+(("dash.el" . "721436b04da4e2795387cb48a98ac6de37ece0fd")
+ ("el-get" . "f220df34333fdb363b84b28f4ed4a5575341bf45")
+ ("emacs-buttercup" . "e4fb7cd560d27d8879a2c7739ee96946adec2df8")
+ ("emacsmirror-mirror" . "d9919dfe6eede6ff668614b23f64cfef0f954c48")
+ ("gnu-elpa-mirror" . "e59499eeb86979ef2b41f004b11c0e712f6354b3")
+ ("melpa" . "fd3bb4b191bf416dd419c5c76d510c7f5890e673")
+ ("nongnu-elpa" . "c7b774608a8b17b5e95a096317d12fae7dc31b68")
+ ("org" . "8ef6205a560cd3a92f8c5f8fe34953b80121c2cb")
+ ("s.el" . "dda84d38fffdaf0c9b12837b504b402af910d01d")
+ ("straight.el" . "b3760f5829dba37e855add7323304561eb57a3d4"))
+:gamma
