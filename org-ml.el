62deb2e0f469b7ca6caef29809ee0805ed57a031
ndwarshuis <ndwar@yavin4.ch>
2025-04-06 13:12:55 -0400

    ENH bump version

--- org-ml.el
+++ org-ml.el
@@ -5,8 +5,8 @@
 ;; Author: Nathan Dwarshuis <ndwar@yavin4.ch>
 ;; Keywords: org-mode, outlines
 ;; Homepage: https://github.com/ndwarshuis/org-ml
-;; Package-Requires: ((emacs "27.1") (org "9.3") (dash "2.17") (s "1.12"))
-;; Version: 5.8.8
+;; Package-Requires: ((emacs "27.1") (org "9.7") (dash "2.17") (s "1.12"))
+;; Version: 6.0.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
